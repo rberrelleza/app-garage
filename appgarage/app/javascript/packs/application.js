765031a4829d84d9fe67ae4cbba6c2e66b32db55
Ramiro Berrelleza <rberrelleza@gmail.com>
2019-07-10 10:55:16 +0200

    Add search component to the frontend

--- appgarage/app/javascript/packs/application.js
+++ appgarage/app/javascript/packs/application.js
@@ -15,3 +15,10 @@
 //
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
+
+import ReactOnRails from 'react-on-rails';
+import Search from '../components/Search';
+
+ReactOnRails.register({
+  Search
+});
